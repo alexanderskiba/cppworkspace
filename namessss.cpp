e804d0d991ae3d5f8e026c597c60f6e253c22fa9
alexanderskiba <58416654+alexanderskiba@users.noreply.github.com>
2020-01-24 15:35:05 +0300

    Add files via upload

---
+++ namessss.cpp
@@ -0,0 +1,93 @@
+//============================================================================
+// Name        : namessss.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+class Person {
+public:
+  void ChangeFirstName(int year, const string& first_name)  {
+    if (first_name.size() > 0) fio_name[year] = first_name;
+                                                            }
+
+  void ChangeLastName(int year, const string& last_name)    {
+    if (last_name.size() > 0) fio_surname[year] = last_name;
+                                                            }
+
+  string GetFullName(int year)  {
+      	string  result_name = "";
+	    string  result_surname = "";
+
+	    if ((fio_name.size() == 0) && (fio_surname.size() == 0))    {
+	        return "Incognito";
+	                                                            }
+	   else {
+        //nothing
+        if (year < fio_name.begin()->first && year < fio_surname.begin()->first)    {
+            return "Incognito";
+                                                                                    }
+        else    {
+            for (auto item : fio_name)  {
+        if (item.first <= year) { result_name = item.second; }
+                                        }
+            for (auto item : fio_surname)   {
+        if (item.first <= year) { result_surname = item.second; }
+                                            }
+
+        //no name
+        if ((result_name.size() == 0) && (result_surname.size() != 0))  {
+            return (result_surname + " with unknown first name");
+                                                                        }
+        //no last_name
+        //else if ((result_name.size() != 0) && (result_surname.size() == 0))
+        else if (fio_surname.empty()|| year< fio_surname.begin() ->first)	{
+            return (result_name + " with unknown last name");
+                                                                            }
+        //all OK
+        else if ((result_name.size() != 0) && (result_surname.size() != 0)) {
+            return (result_name + " " + result_surname);              }
+                }
+            }
+                                    }
+
+private:
+    map <int, string> fio_name;
+    map <int, string> fio_surname;
+};
+
+
+int main() {
+	Person person;
+
+	cout << person.GetFullName(2000) << endl;
+
+	person.ChangeLastName(1968, "1968_2nd");
+	person.ChangeLastName(1967, "1967_2nd");
+	person.ChangeLastName(1965, "1965_2nd");
+	person.ChangeLastName(1966, "1966_2nd");
+
+	for (int year : {1900, 1920, 1950, 1965, 1966, 1967, 1968}) {
+		cout << person.GetFullName(year) << endl;
+	}
+
+	person.ChangeFirstName(1920, "1920_1st");
+	person.ChangeFirstName(1900, "1900_1st");
+	person.ChangeFirstName(1965, "1965_1st");
+	person.ChangeFirstName(1950, "1950_1st");
+
+	for (int year : {1900, 1920, 1950, 1965, 1966, 1967, 1968}) {
+		cout << person.GetFullName(year) << endl;
+	}
+
+
+	return 0;
+}
+
